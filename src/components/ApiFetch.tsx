6ca18061c70cf1867a340a9e3160a78807abc76e
Matt Holmes <m4ttholmes@gmail.com>
2021-12-10 16:13:05 -0500

    initial  scaffolding

---
+++ src/components/ApiFetch.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Results from "./Results";
+
+const ApiFetch = () => {
+
+    return(
+        <div>
+            <Results />
+        </div>
+    )
+}
+
+
+export default ApiFetch;
---
+++ src/components/Results.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+const Results = () => {
+
+
+
+    return(
+        <div>
+            <h1>Results Display</h1>
+        </div>
+    )
+}
+
+export default Results
